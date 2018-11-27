ab42c5ac3f6bca85ae61351849e72bf642643c14
jeremymarques <44778568+jeremymarques@users.noreply.github.com>
2018-11-27 12:06:48 +0100

    Javascript-lesson 1 (#14)
    * Javascript-lesson 1
    
    * exercice 2
    
    * exercice 2

---
+++ javascript/lesson1/exercise1/exercice/main.js
@@ -0,0 +1,12 @@
+var toto = "bonjour"
+console.log(toto);
+toto= 1+1;
+console.log(toto);
+toto= 5-2;
+console.log(toto);
+toto= 2*2;
+console.log(toto);
+toto= 10/2;
+console.log(toto);
+toto= 23%2;
+console.log(toto);
---
+++ javascript/lesson1/exercise2/exercise /main.js
@@ -0,0 +1,35 @@
+var nb1=7;
+var nb2=5;
+var nb3 = nb1 + nb2;
+var somme = "Le resulat de " + nb1 + "+" + nb2 + " est " + nb3;
+console.log(somme);
+
+var nb4=8;
+var nb5=6;
+var nb6= nb4 - nb5;
+var resultat = "Le resultat de " + nb4 + "-" + nb5 + " est " + nb6;
+console.log(resultat);
+
+var nb7=9;
+var nb8=4;
+var nb9= nb7 * nb8;
+var multiplication = "Le resultat de " + nb7 + "x" + nb8 + " est " + nb9;
+console.log(multiplication);
+
+var nb10=11;
+var nb11=34;
+var nb12= nb10 / nb11;
+var divison = "Le resultat de " + nb10 + "/" + nb11 + " est " + nb12;
+console.log(divison);
+
+var nb13=77;
+var nb14=15;
+var nb15= nb13 % nb14;
+var modulo = "Le resultat de " + nb13 + "%" + nb14 + " est " + nb15;
+console.log(modulo);
+
+var lettre="J";
+var nb16=34;
+var nb17= lettre + nb16;
+var addition = "Le resultat de " + lettre + "+" + nb16 + " est " + nb17;
+console.log(addition);
