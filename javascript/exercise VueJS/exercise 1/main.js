b0137f539a0d5529493bf7f79d44f55e77dcffc0
jérémy marques <jeremy-jeje75@hotmail.fr>
2018-12-18 11:24:45 +0100

    VueJS - exercise 1

---
+++ javascript/exercise VueJS/exercise 1/main.js
@@ -0,0 +1,12 @@
+var test = new Vue({
+    el: '#app',
+    data: function() {
+      return {
+        profs: {
+          Alex: ['Lundi','Mardi','Mercredi','Vendredi'],
+          Erwan: ['Vendredi'],
+          Marin: ['Lundi','Mardi','Mercredi','Jeudi']
+        }
+      }
+    }
+  });
